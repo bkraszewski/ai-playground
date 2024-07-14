47367b99d7a0e71d387453dbfc78d89a8680af71
bkraszewski <bkraszewski@gmail.com>
2024-07-14 20:19:13 +0200

    refactor, better preprocessing

--- kaggle/titanic.py
+++ kaggle/titanic.py
@@ -14,23 +14,63 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import matplotlib.pyplot as plt
+import re
+
+use_neural_net = True
+should_show_plot = True
 
 
 def prepare_data(df):
-    df = df.drop(["Ticket", "Name", "Cabin"], axis=1)
+
     df["Age"] = df["Age"].fillna(df["Age"].median())
 
     df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
 
-    # Encode the 'Sex' column manually
-    df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
-    df = pd.get_dummies(df, columns=["Embarked"])
-
     # calculate family size
     df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+    df["IsAlone"] = 0
+    df.loc[df["FamilySize"] == 1, "IsAlone"] = 1
 
-    df = df.astype("float32")
-    return df
+    # age group
+    df.loc[df["Age"] <= 15, "Age"] = 0
+    df.loc[(df["Age"] > 15) & (df["Age"] <= 25), "Age"] = 1
+    df.loc[(df["Age"] > 25) & (df["Age"] <= 35), "Age"] = 2
+    df.loc[(df["Age"] > 35) & (df["Age"] <= 45), "Age"] = 3
+    df.loc[(df["Age"] > 45) & (df["Age"] <= 60), "Age"] = 4
+    df.loc[df["Age"] > 60, "Age"] = 5
+
+    # title
+    df["Title"] = df["Name"].apply(get_title)
+    df["Title"] = df["Title"].replace(
+        ["Capt.", "Dr.", "Major.", "Rev.", "Col."], "Officer."
+    )
+    df["Title"] = df["Title"].replace(
+        ["Lady.", "Countess.", "Don.", "Sir.", "Jonkheer.", "Dona."], "Royal."
+    )
+    df["Title"] = df["Title"].replace(["Mlle.", "Ms."], "Miss.")
+    df["Title"] = df["Title"].replace(["Mme."], "Mrs.")
+
+    # cabin type
+    df["Cabin"] = df["Cabin"].fillna("Missing")
+    df["Cabin"] = df["Cabin"].str[0]
+
+    df = df.drop(["Name", "Ticket"], axis=1)
+    all_dummies = pd.get_dummies(
+        df, drop_first=True, columns=["Embarked", "Title", "Cabin", "Sex"]
+    )
+
+    all_dummies = all_dummies.astype("float32")
+    print(all_dummies.head())
+    return all_dummies
+
+
+def get_title(name):
+    title_search = re.search(" ([A-Za-z]+\.)", name)
+
+    if title_search:
+        return title_search.group(1)
+    return ""
+
 
 def show_plot(history):
     plt.figure(figsize=(12, 5))
@@ -56,35 +96,45 @@ def show_plot(history):
     plt.show()
 
 
+def neural_net(train_dataset, should_show_plot, val_dataset):
+    model = Sequential(
+        [
+            Dense(
+                units=X_train.shape[1],
+                input_shape=(X_train.shape[1],),
+                activation="relu",
+            ),
+            Dense(units=1, activation="sigmoid"),
+        ]
+    )
+
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+    history = model.fit(train_dataset, epochs=50, validation_data=val_dataset)
+
+    if should_show_plot:
+        show_plot(history)
+
+    predictions = model.predict(test_dataset)
+    predictions = (predictions > 0.5).astype(int)
+    predictions = predictions.ravel()
+    return predictions
+
+
 train_df = prepare_data(pd.read_csv("train.csv"))
 
 X = train_df.drop("Survived", axis=1)
 y = train_df["Survived"]
 
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(
+    X, y, test_size=0.1, random_state=42
+)
 
 train_dataset = tf.data.Dataset.from_tensor_slices((X_train.values, y_train.values))
 val_dataset = tf.data.Dataset.from_tensor_slices((X_val.values, y_val.values))
 
 train_dataset = train_dataset.shuffle(len(X_train)).batch(32)
 val_dataset = val_dataset.batch(32)
-
-model = Sequential(
-    [
-        Dense(
-            units=X_train.shape[1], input_shape=(X_train.shape[1],), activation="relu"
-        ),
-        Dense(units=1, activation="sigmoid"),
-    ]
-)
-
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-history = model.fit(train_dataset, epochs=50, validation_data=val_dataset)
-
-val_loss, val_acc = model.evaluate(val_dataset)
-
-show_plot(history)
 
 
 # test part
@@ -98,12 +148,10 @@ test_df = test_df[X_train.columns]
 
 test_dataset = tf.data.Dataset.from_tensor_slices(test_df.values).batch(32)
 
-predictions = model.predict(test_dataset)
-predictions = (predictions > 0.5).astype(int)
-predictions = predictions.ravel()
+if use_neural_net:
+    predictions = neural_net(train_dataset, should_show_plot, val_dataset)
+    ids = pd.read_csv("test.csv")["PassengerId"]
 
-ids = pd.read_csv("test.csv")["PassengerId"]
-
-output = pd.DataFrame({"PassengerId": ids, "Survived": predictions.ravel()})
-# print(output.head())
-output.to_csv("submission.csv", index=False)
+    output = pd.DataFrame({"PassengerId": ids, "Survived": predictions.ravel()})
+    # print(output.head())
+    output.to_csv("submission.csv", index=False)
